dbff9d29b89ae83bc2bdaee7a3056c274d280478
Dave <davegt40@gmail.com>
2018-12-29 15:10:17 -0500

    move base_dir

--- website/settings.py
+++ website/settings.py
@@ -14,6 +14,9 @@ import json
 import os
 from django.core.exceptions import ImproperlyConfigured
 
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 with open(os.path.join(BASE_DIR, 'secrets.json')) as secrets_file:
     secrets = json.load(secrets_file)
 
@@ -21,12 +24,8 @@ def get_secret(setting, secrets=secrets):
     """Get secret setting or fail with ImproperlyConfigured"""
     try:
         return secrets[setting]
-    except: KeyError:
+    except KeyError:
         raise ImproperlyConfigured("Set the {} setting".format(setting))
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
