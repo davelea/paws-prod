56f22dd9852560b671d88d2659612d35fb45721c
Dave <davegt40@gmail.com>
2018-12-30 14:33:44 -0500

    create blog model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+	title = models.CharField(max_length=255)
+	url = models.CharField(max_length=255)
+	published_date = models.DateTimeField()
+	summary = models.CharField(max_length=1000)
+	body = models.TextField()
+	image = models.ImageField(upload_to='images/', blank=True)
