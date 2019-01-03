7a34e8c726c84fd0deb794ca6aad84c2a1bb7ab2
Dave <davegt40@gmail.com>
2019-01-02 23:40:05 -0500

    fix indentation

--- base/views.py
+++ base/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 def home(request):
 	return render(request, 'base/index.html')
 
-	def pricing(request):
+def pricing(request):
 	return render(request, 'base/pricing.html')
