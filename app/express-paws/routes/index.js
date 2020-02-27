05470c94c7a8bae8b09092eeef07f7731d3928d3
Dave <davegt40@gmail.com>
2020-02-26 23:30:06 -0500

    base routing

--- app/express-paws/routes/index.js
+++ app/express-paws/routes/index.js
@@ -3,7 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Paws Around Motown'} );
+});
+
+router.get('/pricing', function(req, res, next) {
+  res.render('pricing');
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about');
+});
+
+router.get('/team', function(req, res, next) {
+  res.render('team');
+});
+
+router.get('/faq', function(req, res, next) {
+  res.render('faq');
+});
+
+router.get('/request-more-information', function(req, res, next) {
+  res.render('rfi');
 });
 
 module.exports = router;
