7eb95a724e8426235a14f4c714e09086cc684453
Dave <davegt40@gmail.com>
2020-03-23 21:39:27 -0400

    pass titles

--- app/express-paws/routes/index.js
+++ app/express-paws/routes/index.js
@@ -7,23 +7,23 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/pricing', function(req, res, next) {
-  res.render('pricing');
+  res.render('pricing', { title: 'Paws Around Motown: Pricing'});
 });
 
 router.get('/about', function(req, res, next) {
-  res.render('about');
+  res.render('about', { title: 'Paws Around Motown: About Us'});
 });
 
 router.get('/team', function(req, res, next) {
-  res.render('team');
+  res.render('team', { title: 'Paws Around Motown: Our Team'});
 });
 
 router.get('/faq', function(req, res, next) {
-  res.render('faq');
+  res.render('faq', { title: 'Paws Around Motown: FAQ'});
 });
 
 router.get('/request-more-information', function(req, res, next) {
-  res.render('request-more-information');
+  res.render('request-more-information', { title: 'Paws Around Motown: Request More Information'});
 });
 
 module.exports = router;
