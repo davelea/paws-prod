48138fb41f2e690174a6f5770a9fdfd358fa9f4c
Dave <davegt40@gmail.com>
2020-03-31 20:51:00 -0400

    setup mongoose db

--- app/express-paws/app.js
+++ app/express-paws/app.js
@@ -3,11 +3,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+let mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// set up mongoose connection
+let mongoDB = 'mongodb://127.0.0.1/paws';
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
