b6c428cbad96a15a4d4be435aad4858c9029c1e6
davelea <davegt40@gmail.com>
2020-07-23 01:09:51 -0400

    rotate chevron

--- public/javascript/pricing.js
+++ public/javascript/pricing.js
@@ -8,12 +8,18 @@ function addPricingEventListeners() {
 
 function togglePricingVisibility(event) {
   console.log(event.currentTarget.parentElement);
+  // TODO: use closest or something cleaner
   const contentSection = event.currentTarget.parentElement.parentElement.querySelector('.pricing-list__content');
+
+  const chevron = event.currentTarget.querySelector('.pricing-list__summary-action > i')
 
   if (contentSection.classList.contains('paws-hide')) {
     contentSection.classList.remove('paws-hide');
+
+    chevron.classList.add('paws-rotate-90');
   } else {
     contentSection.classList.add('paws-hide');
+    chevron.classList.remove('paws-rotate-90');
   }
 }
 
