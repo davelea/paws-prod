d5f165b24a2bab89740ce07e7a3681166a50c056
Dave Lea <davegt40@gmail.com>
2021-04-01 21:50:23 -0400

    add tailwind to home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,18 @@ module.exports = {
     extend: {},
     fontFamily: {
       'sans': ['Open Sans', 'sans-serif']
+    },
+    fontSize: {
+      'xs': ['.75rem', '1rem'],
+      'sm': ['.875rem', '1.25rem'],
+      'base': ['1rem', '1.5rem'],
+      'lg': ['1.125rem', '1.75rem'],
+      'xl': ['1.25rem', '1.75rem'],
+      '2xl': ['1.5rem', '2rem'],
+      '3xl': ['2rem', '1.2'],
+      '4xl': ['2.5rem', '1.2'],
+      '5xl': ['3rem', '1'],
+      '6xl': ['4rem', '1']
     }
   },
   variants: {
