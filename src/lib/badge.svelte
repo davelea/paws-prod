d6f697a9d3c2b475c3d42e977c579628eed701ae
Dave Lea <davegt40@gmail.com>
2023-02-02 21:37:20 -0500

    badge color

--- src/lib/badge.svelte
+++ src/lib/badge.svelte
@@ -1,8 +1,10 @@
 <script>
   export let label;
-  export let color = green;
 </script>
 
-<span style="background-color: #7D82B8; color: #fff" class="font-medium px-4 py-2.5 rounded-full">
+<span
+  style="background-color: rgba(229, 211, 21, 0.40);"
+  class="font-medium px-4 py-2.5 rounded-full"
+>
   {label}
 </span>
