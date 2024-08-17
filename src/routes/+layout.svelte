731a0ff9a0edc9c6adac04f8219658e9d867c860
Dave Lea <davegt40@gmail.com>
2024-08-17 19:04:59 -0400

    remove unused script

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -34,13 +34,6 @@
 
   <link rel="stylesheet" href="/styles/paws-styles.css" />
 
-  <!-- <link
-    rel="stylesheet"
-    href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
-    integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
-    crossorigin="anonymous"
-  /> -->
-
   <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
   <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
   <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
